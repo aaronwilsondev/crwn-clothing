ddff0bcf0596a39093e16b73e5ff9f90fcc38ad8
Aaron Wilson <aaronwilson1234@outlook.ie>
2021-01-09 16:03:29 +0000

    start implimenting code for firestore database

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import HomePage from "./pages/homepage/homepage.component.jsx";
 import ShopPage from "./pages/shop/shop.component.jsx";
 import Header from './components/header/header.component.jsx';
 import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx";
-import { auth } from "./firebase/firebase.utils";
+import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
 
 
 class App extends React.Component {
@@ -17,14 +17,13 @@ class App extends React.Component {
    }
  }
 
- unsubscribeFromAuth = null
+ unsubscribeFromAuth = null;
 
   componentDidMount(){
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({ currentUser: user });
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async user => {
+      createUserProfileDocument(user);
 
-      console.log(user)
-    })
+      });
   }
 
   componentWillUnmount() {
