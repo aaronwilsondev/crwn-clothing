e076d355df4f1e6aa1c2d185b09c6a6388020594
Aaron Wilson <aaronwilson1234@outlook.ie>
2021-01-16 16:49:15 +0000

    implementing cartStructredSelector onto mapStateToProps, adding empty cart message to dropdown component

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ import ShopPage from "./pages/shop/shop.component.jsx";
 import Header from './components/header/header.component.jsx';
 import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx";
 import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
-
+import { selectCurrentUser } from "./redux/user/user.selectors";
+import { createStructuredSelector } from 'reselect';
 
 class App extends React.Component {
 
@@ -57,8 +58,8 @@ const {setCurrentUser } = this.props
   }
 }
 
-const mapStateToProps = ({ user }) => ({
-  currentUser: user.currentUser
+const mapStateToProps = createStructuredSelector({
+  currentUser: selectCurrentUser
 });
 
 const mapDispatchToProps = dispatch => ({
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 import { connect } from "react-redux";
+import { createStructuredSelector } from "reselect";
+
+
 import CartIcon from "../cart-icon/cart-icon.component";
 import "./header.styles.scss";
 import { Link } from "react-router-dom";
 import { auth } from "../../firebase/firebase.utils";
 import { ReactComponent as Logo } from "../../assets/crown.svg";
 import CartDropdown from "../cart-dropdown/cart-dropdown.component";
+import { selectCartHidden } from "../../redux/cart/cart.selectors";
+import { selectCurrentUser } from "../../redux/user/user.selectors";
 
 const Header = ({ currentUser, hidden }) => (
     <div className="header">
@@ -35,9 +40,11 @@ const Header = ({ currentUser, hidden }) => (
     </div>
 );
 
-const mapStateToProps = ({user: { currentUser }, cart:{ hidden } }) => ({
-currentUser,
-hidden
-})
+const mapStateToProps = createStructuredSelector({
+currentUser: selectCurrentUser,
+hidden: selectCartHidden
+});
+
+
 
 export default connect(mapStateToProps)(Header);
