e1cee8bd6b256d9c0489267f9fddf38a23fcc640
Aaron Wilson <aaronwilson1234@outlook.ie>
2021-02-03 16:17:39 +0000

    begin implementing redux saga into project, tansfered aysnc event handling of shop data request from thunk to saga

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,18 +1,24 @@
 import { createStore, applyMiddleware } from "redux";
 import logger from "redux-logger";
 import { persistStore } from "redux-persist";
+import createSagaMiddleware from "redux-saga";
 
 import rootReducer from "./root-reducer";
 
-import thunk from "redux-thunk";
+import { fetchCollectionsStart } from "./shop/shop.sagas"; 
 
-const middlewares = [thunk];
+const sagaMiddleware = createSagaMiddleware();
+
+const middlewares = [sagaMiddleware];
 
 if (process.env.NODE_ENV === "development") {
     middlewares.push(logger);
 }
 
-export const store = createStore(rootReducer, applyMiddleware(...middlewares))
+export const store = createStore(rootReducer, applyMiddleware(...middlewares));
+
+sagaMiddleware.run(fetchCollectionsStart);
+
 
 export const persistor = persistStore(store);
 
